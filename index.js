a46fa1ed3b926bd70420255a13684b9760dd0811
hoangthang3010 <thangminhhoang98@gmail.com>
2024-03-22 11:39:23 +0700

    update

--- index.js
+++ index.js
@@ -20,9 +20,54 @@ const server = app.listen(PORT, () => {
 });
 
 
-app.get("/", (req, res) => {
-  console.log(req.query);
-  res.send({
-    url: req.query.url
-  });
+app.get("/", async (req, res) => {
+  const url = req.query.url
+  const {
+    ndown,
+    tikdown,
+    ytdown,
+    twitterdown,
+  } = require("nayan-media-downloader");
+  let data = {
+    url: null,
+    thumbnail: null,
+    type: null,
+  };
+  if (url.includes("facebook") || url.includes("instagram")) {
+    downloading(msg);
+    const res = await ndown(url);
+    data = {
+      ...data,
+      type: "fb",
+      url: res.data[0].url || data.data[1].url,
+      thumbnail: res.data[0].thumbnail,
+    };
+  } else if (url.includes("youtube")) {
+    downloading(msg);
+    const res = await ytdown(url);
+    data = {
+      ...data,
+      type: "ytb",
+      url: res.data.video,
+      thumbnail: res.data.picture,
+    };
+  } else if (url.includes("tiktok")) {
+    downloading(msg);
+    const res = await tikdown(url);
+    console.log(res);
+    data = {
+      ...data,
+      type: "tik",
+      url: res.data.video,
+    };
+  } else if (url.includes("twitter")) {
+    downloading(msg);
+    const res = await twitterdown(url);
+    data = {
+      ...data,
+      type: "twi",
+      url: res.data.HD || res.data.SD,
+    };
+  }
+  res.send(data);
 });
